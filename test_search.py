23f900a4ec23bfaa58bbe9ed7dda778ff2795ebf
TARScn <luocx0301@gmail.com>
2026-07-03 13:53:06 +0800

    删除多余脚本测试代码

--- test_search.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, urllib.parse, json
-kw = '海底捞'
-url = 'http://localhost:8080/shop/search?keyword=' + urllib.parse.quote(kw)
-r = urllib.request.urlopen(url, timeout=5)
-data = json.loads(r.read().decode())
-print(f'success={data["success"]}, count={len(data["data"])}')
-for d in data['data']:
-    print(f'  {d["id"]} | {d["name"]}')
